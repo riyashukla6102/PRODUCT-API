5e4a49ed2729aaba8f55af6bce6a4377ac463a31
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2023-07-31 17:49:24 +0530

    Fetch or GET data from db
    Signed-off-by: riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>

--- server.js
+++ server.js
@@ -25,6 +25,25 @@ app.post('/product',async(req,res)=>{
         res.status(500).json({message:error.message})
     }
 })
+
+app.get('/products', async(req,res)=>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+       }catch(error){
+        res.status(500).json({message:error.message})
+    }
+} )
+
+app.get('/products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params; 
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
 mongoose.
 connect('mongodb+srv://2019riyashukla:123456789RIYA@devapi.7ouxclk.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(()=>{
