86cf8421c8ff7d99d6919dcafa81804a285740ba
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2023-07-31 16:35:25 +0530

    Create Model for Data in Db
    Signed-off-by: riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.get('/',(req,res)=>{
 app.get("/blog",(req,res)=>{
     res.send("Hello blog my name is dev")
 })
-app.listen(3000,()=>{
-    console.log('Node api is running on port 3000')
-})
+
 
 mongoose.
 connect('mongodb+srv://2019riyashukla:123456789RIYA@devapi.7ouxclk.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(()=>{
-    console.log("connected to mongoDB")
+    app.listen(3000,()=>{
+        console.log('Node api is running on port 3000')
+    })
 }).catch((error)=>{
     console.log(error)
 })
