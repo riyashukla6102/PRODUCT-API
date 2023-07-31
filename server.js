f9daa1f9a220a5a452b099359a2541bc0ff37661
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2023-07-31 14:48:20 +0530

    Connect database - MongoDB
    Signed-off-by: riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app =express()
 
 //routes
@@ -6,6 +7,18 @@ const app =express()
 app.get('/',(req,res)=>{
     res.send('hello node api')
 })
+
+app.get("/blog",(req,res)=>{
+    res.send("Hello blog my name is dev")
+})
 app.listen(3000,()=>{
     console.log('Node api is running on port 3000')
+})
+
+mongoose.
+connect('mongodb+srv://2019riyashukla:123456789RIYA@devapi.7ouxclk.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected to mongoDB")
+}).catch((error)=>{
+    console.log(error)
 })
